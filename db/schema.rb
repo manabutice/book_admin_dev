0e4d66e28f3e225120f99768598ecd68a0f189de
村上学 <>
2024-03-26 12:04:32 +0900

    モデル同士のリレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_25_021012) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_26_011002) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.string "penname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "book_authors", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_book_authors_on_author_id"
+    t.index ["book_id"], name: "index_book_authors_on_book_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -35,5 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_25_021012) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "book_authors", "authors"
+  add_foreign_key "book_authors", "books"
   add_foreign_key "books", "publishers"
 end
