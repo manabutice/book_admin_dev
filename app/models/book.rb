69b3f937d06dc93f1a241d0ef4fc72cfba3d2a34
村上学 <>
2024-03-31 09:39:22 +0900

    ActiveRecord：：Enumで列挙型を扱う

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,12 @@
 class Book < ApplicationRecord
+  enum sales_status: {
+    reservation: 0,
+    now_on_sale: 1,
+    end_of_print: 2,
+  }
   scope :costly, -> { where("price > ?", 3000)}
   scope :written_about, ->(theme) { where("name like ?", "%#{theme}%")}
+  scope :find_price, ->(price) { find_by(price: price)}
 
   belongs_to :publisher
   has_many :book_authors
@@ -13,5 +19,23 @@ class Book < ApplicationRecord
     if book.name.include?("exercise")
       book.errors[:name] << "I don't like exercise."
     end
+  end
+
+  before_validation do
+    self.name = self.name.gsub(/Cat/) do |matched|
+      "lovely #{matched}"
+    end
+  end
+  after_destroy do
+    Rails.logger.info "Book is deleted: #{self.attributes}"
+  end
+
+  after_destroy :if => :high_price? do
+    Rails.logger.warn "Book with high price is deleted: #{self.attributes}"
+    Rails.logger.warn "Please check!!"
+  end
+
+  def high_price?
+    price >= 5000
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_26_011002) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_30_110928) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.string "penname"
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_26_011002) do
     t.date "published_on"
     t.integer "price"
     t.integer "publisher_id"
+    t.integer "sales_status"
     t.index ["publisher_id"], name: "index_books_on_publisher_id"
   end
 
