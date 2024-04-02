1634ed1388548571ae3af7b3359226a8780f2f84
村上学 <>
2024-04-02 10:39:56 +0900

    アクションに対してフックで処理を差し込む

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,30 @@
 class BooksController < ApplicationController
-  def show
-    @book = Book.find(params[:id])
-    respond_to do |format|
-      format.html
-      format.json
+    def show
+      @book = Book.find(params[:id])
+      respond_to do |format|
+        format.html
+        format.json
+      end
     end
+    #destroyメソッドの追加
+    def destroy
+      @book = Book.find(params[:id])
+      @book.destroy
+      respond_to do |format|
+        format.html { redirect_to "/" }
+        format.json { head :no_content }
+      end
+    end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def action_logger
+    logger.info "around-before"
+    yield
+    logger.info "around-after"
   end
 end
